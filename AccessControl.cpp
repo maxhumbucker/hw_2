5f2f6ecc5e298c598e43405ebabb8ac26436cc64
Max <131478866+maxhumbucker@users.noreply.github.com>
2023-04-27 19:42:17 +0300

    Update AccessControl.cpp

--- AccessControl.cpp
+++ AccessControl.cpp
@@ -110,6 +110,30 @@ ResposeData AccessControl::AC_GrantModeratorRole(int sender, int user)
 	response.Response = "";
 	response.Success = false;
 
+	auto Sender = Database.find(sender);
+	auto User = Database.find(user);
+
+	if (Sender == Database.end() || User == Database.end())
+	{
+		response.Response = "ERROR: No such sender or user!";
+		return response;
+	}
+
+	if (Sender->second != ROLE::Admin || Sender->second != ROLE::Moderator)
+	{
+		response.Response = "ERROR: Sender isn't admin or moderator!";
+		return response;
+	}
+
+	if (User->second == ROLE::Admin || User->second == ROLE::Moderator)
+	{
+		response.Response = "ERROR: User is admin or moderator!";
+		return response;
+	}
+
+	User->second = ROLE::Moderator;
+	response.Response = "User " + std::to_string(user) + " is moderator.";
+	response.Success = true;
 	return response;
 }
 
@@ -118,6 +142,30 @@ ResposeData AccessControl::AC_GrantAdminRole(int sender, int user)
 	ResposeData response;
 	response.Response = "";
 	response.Success = false;
+	auto Sender = Database.find(sender);
+	auto User = Database.find(user);
+	
+		if (Sender == Database.end() || User == Database.end())
+	{
+		response.Response = "ERROR: No such sender or user!";
+		return response;
+	}
+
+	if (Sender->second != ROLE::Admin)
+	{
+		response.Response = "ERROR: Sender isn't admin!";
+		return response;
+	}
+
+	if (User->second == ROLE::Admin)
+	{
+		response.Response = "ERROR: User is admin!";
+		return response;
+	}
+
+	User->second = ROLE::Admin;
+	response.Response = "User " + std::to_string(user) + " is admin.";
+	response.Success = true;
 
 	return response;
 }
